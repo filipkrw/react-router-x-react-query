e75361faf3d042c411ff2c01c89d334d7d1c228e
Filip Krawczyk <filip@algomo.com>
2023-05-18 16:19:36 +0100

    Types

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -4,13 +4,13 @@ export const UserService = {
       setTimeout(() => {
         resolve({ name: "Filip" });
       }, 1500);
-    });
+    }) as Promise<{ name: string }>;
   },
   getFavoriteColor: async () => {
     return new Promise((resolve) => {
       setTimeout(() => {
         resolve({ color: "blue" });
       }, 2000);
-    });
+    }) as Promise<{ color: string }>;
   },
 };
